7f7c26814c4c2c51726bf3471af73fd7b97a9006
Benjamin Wild <b.w@fu-berlin.de>
2014-11-24 18:45:15 +0100

    Implement processing pipeline selection and basic visualization

--- BeesBookImgAnalysisTracker.cpp
+++ BeesBookImgAnalysisTracker.cpp
@@ -1,21 +1,113 @@
 #include "BeesBookImgAnalysisTracker.h"
 
+#include <chrono>
+#include <vector>
+
+#include <opencv2/core/core.hpp>
+
 #include "source/tracking/algorithm/algorithms.h"
+#include "source/tracking/algorithm/BeesBookImgAnalysisTracker/pipeline/datastructure/Tag.h"
+
+#include "ui_ToolWidget.h"
+#include "ui_ParamsWidget.h"
 
 namespace {
-auto _ = Algorithms::Registry::getInstance().register_tracker_type<BeesBookImgAnalysisTracker>("BeesBook ImgAnalysis");
+auto _ = Algorithms::Registry::getInstance()
+             .register_tracker_type<BeesBookImgAnalysisTracker>(
+                 "BeesBook ImgAnalysis");
+
+struct CursorOverrideRAII {
+    CursorOverrideRAII(Qt::CursorShape shape) { QApplication::setOverrideCursor(shape); }
+    ~CursorOverrideRAII() { QApplication::restoreOverrideCursor(); }
+};
 }
 
-
-BeesBookImgAnalysisTracker::BeesBookImgAnalysisTracker(Settings &settings, std::string &serializationPathName, QWidget *parent)
+BeesBookImgAnalysisTracker::BeesBookImgAnalysisTracker(
+    Settings& settings, std::string& serializationPathName, QWidget* parent)
     : TrackingAlgorithm(settings, serializationPathName, parent)
-{}
+    , _stage(SelectedStage::NoProcessing)
+    , _paramsWidget(std::make_shared<QWidget>())
+    , _toolsWidget(std::make_shared<QWidget>())
+{
+    Ui::ToolWidget uiTools;
+    uiTools.setupUi(_toolsWidget.get());
+    Ui::ParamsWidget uiParams;
+    uiParams.setupUi(_paramsWidget.get());
 
-void BeesBookImgAnalysisTracker::track(ulong frameNumber, cv::Mat &frame)
-{}
+    auto connectRadioButton = [&](QRadioButton* button, SelectedStage stage) {
+        QObject::connect(button, &QRadioButton::toggled,
+                         [=](bool checked) { stageSelectionToogled(stage, checked); });
+    };
 
-void BeesBookImgAnalysisTracker::paint(cv::Mat &image)
-{}
+    connectRadioButton(uiTools.radioButtonNoProcessing, SelectedStage::NoProcessing);
+    connectRadioButton(uiTools.radioButtonConverter, SelectedStage::Converter);
+    connectRadioButton(uiTools.radioButtonLocalizer, SelectedStage::Localizer);
+    connectRadioButton(uiTools.radioButtonRecognizer, SelectedStage::Recognizer);
+    connectRadioButton(uiTools.radioButtonTransformer, SelectedStage::Transformer);
+    connectRadioButton(uiTools.radioButtonGridFitter, SelectedStage::GridFitter);
+    connectRadioButton(uiTools.radioButtonDecoder, SelectedStage::Decoder);
+}
 
-void BeesBookImgAnalysisTracker::reset()
-{}
+void BeesBookImgAnalysisTracker::track(ulong frameNumber, cv::Mat& frame) {
+	std::lock_guard<std::mutex> lock(_tagListLock);
+    CursorOverrideRAII(Qt::WaitCursor);
+
+    _taglist.clear();
+
+    if (_stage < SelectedStage::Converter) return;
+    cv::Mat image = _converter.process(frame);
+    emit notifyGUI("Converter finished", MSGS::NOTIFICATION);
+    if (_stage < SelectedStage::Localizer) return;
+    _taglist = _localizer.process(std::move(image));
+    emit notifyGUI("Localizer finished", MSGS::NOTIFICATION);
+    if (_stage < SelectedStage::Recognizer) return;
+    _recognizer.process(_taglist);
+    emit notifyGUI("Recognizer finished", MSGS::NOTIFICATION);
+    if (_stage < SelectedStage::Transformer) return;
+    _transformer.process(_taglist);
+    emit notifyGUI("Transformer finished", MSGS::NOTIFICATION);
+    if (_stage < SelectedStage::GridFitter) return;
+    _gridFitter.process(_taglist);
+    emit notifyGUI("GridFitter finished", MSGS::NOTIFICATION);
+    if (_stage < SelectedStage::Decoder) return;
+    _decoder.process(_taglist);
+    emit notifyGUI("Decoder finished", MSGS::NOTIFICATION);
+}
+
+void BeesBookImgAnalysisTracker::visualizeLocalizerOutput(cv::Mat& image) const {
+	for (const decoder::Tag& tag : _taglist) {
+		const cv::Rect& box = tag.getBox();
+        cv::rectangle(image, box, cv::Scalar(255, 0, 0), 2);
+	}
+}
+
+void BeesBookImgAnalysisTracker::visualizeRecognizerOutput(cv::Mat& image) const {
+    for (const decoder::Tag& tag : _taglist) {
+        for (const decoder::TagCandidate& candidate : tag.getCandidates()) {
+            const decoder::Ellipse& ell{candidate.getEllipse()};
+            //TODO
+        }
+    }
+}
+
+void BeesBookImgAnalysisTracker::paint(cv::Mat& image) {
+	// don't try to visualize results while data processing is running
+	if (_tagListLock.try_lock()) {
+		visualizeLocalizerOutput(image);
+		visualizeRecognizerOutput(image);
+		_tagListLock.unlock();
+	} else {
+		return;
+	}
+}
+
+void BeesBookImgAnalysisTracker::reset() {}
+
+void BeesBookImgAnalysisTracker::stageSelectionToogled(BeesBookImgAnalysisTracker::SelectedStage stage, bool checked)
+{
+    if (checked) {
+        _stage = stage;
+        emit forceTracking();
+    }
+}
+
--- BeesBookImgAnalysisTracker.h
+++ BeesBookImgAnalysisTracker.h
@@ -1,17 +1,27 @@
 #ifndef BeesBookImgAnalysisTracker_H
 #define BeesBookImgAnalysisTracker_H
 
+#include <mutex>
+
 #include <opencv2/opencv.hpp>
+
+#include <QApplication>
 
 #include "source/settings/Settings.h"
 #include "source/tracking/TrackingAlgorithm.h"
+#include "source/tracking/algorithm/BeesBookImgAnalysisTracker/pipeline/Decoder.h"
+#include "source/tracking/algorithm/BeesBookImgAnalysisTracker/pipeline/Recognizer.h"
+#include "source/tracking/algorithm/BeesBookImgAnalysisTracker/pipeline/GridFitter.h"
+#include "source/tracking/algorithm/BeesBookImgAnalysisTracker/pipeline/Converter.h"
+#include "source/tracking/algorithm/BeesBookImgAnalysisTracker/pipeline/Transformer.h"
+#include "source/tracking/algorithm/BeesBookImgAnalysisTracker/pipeline/Localizer.h"
 
 class BeesBookImgAnalysisTracker : public TrackingAlgorithm {
 	Q_OBJECT
   public:
 	BeesBookImgAnalysisTracker(Settings& settings, std::string& serializationPathName, QWidget* parent);
 
-	void track(ulong frameNumber, cv::Mat& frame) override;
+    void track(ulong frameNumber, cv::Mat& frame) override;
 	void paint(cv::Mat& image) override;
 	void reset() override;
 
@@ -26,8 +36,35 @@ class BeesBookImgAnalysisTracker : public TrackingAlgorithm {
 	void mouseWheelEvent(QWheelEvent*) override {}
 
   private:
-	const std::shared_ptr<QWidget> _paramsWidget;
-	const std::shared_ptr<QWidget> _toolsWidget;
+    enum class SelectedStage : uint8_t {
+        NoProcessing = 0,
+        Converter,
+        Localizer,
+        Recognizer,
+        Transformer,
+        GridFitter,
+        Decoder
+    };
+    SelectedStage _stage;
+
+    const std::shared_ptr<QWidget> _paramsWidget;
+    const std::shared_ptr<QWidget> _toolsWidget;
+
+	decoder::Converter _converter;
+	decoder::Decoder _decoder;
+	decoder::GridFitter _gridFitter;
+	decoder::Recognizer _recognizer;
+	decoder::Transformer _transformer;
+	decoder::Localizer _localizer;
+
+	std::vector<decoder::Tag> _taglist;
+	std::mutex _tagListLock;
+
+	void visualizeLocalizerOutput(cv::Mat& image) const;
+	void visualizeRecognizerOutput(cv::Mat& image) const;
+
+private slots:
+    void stageSelectionToogled(SelectedStage stage, bool checked);
 };
 
 #endif
