121d61a22333b46a99a85f88e2d08788a5c9e73f
Mareike Ziese <mareike.ziese@gmx.net>
2015-03-19 14:13:54 +0100

    fix mergebugs

--- EllipseFitterParamsWidget.cpp
+++ EllipseFitterParamsWidget.cpp
@@ -1,13 +1,13 @@
-#include "RecognizerParamsWidget.h"
+#include "EllipseFitterParamsWidget.h"
 
 #include "source/settings/Settings.h"
 #include "Common.h"
 
-#include "ui_RecognizerParamsWidget.h"
+#include "ui_EllipseFitterParamsWidget.h"
 
-using namespace pipeline::settings::Recognizer;
+using namespace pipeline::settings::EllipseFitter;
 
-RecognizerParamsWidget::RecognizerParamsWidget(Settings &settings)
+EllipseFitterParamsWidget::EllipseFitterParamsWidget(Settings &settings)
 : ParamsSubWidgetBase(settings)
 	/*, _cannyThresholdLowSlider(this, &_layout, "Canny Mean Min", 0, 200,0, 1)
 	, _cannyThresholdHighSlider(this, &_layout, "Canny Mean Max", 0, 200,0, 1)
@@ -20,7 +20,7 @@ RecognizerParamsWidget::RecognizerParamsWidget(Settings &settings)
 	, _thresholdVoteSlider(this, &_layout, "Threshold Best Vote", 0, 5000,0, 1)*/
 {
 
-	Ui::RecognizerParamsWidget paramsWidget;
+	Ui::EllipseFitterParamsWidget paramsWidget;
 	paramsWidget.setupUi(this);
 
 	/**
@@ -28,16 +28,16 @@ RecognizerParamsWidget::RecognizerParamsWidget(Settings &settings)
 	 */
 	connectSettingsWidget(paramsWidget.canny_inital_high,
 				Params::BASE + Params::CANNY_INITIAL_HIGH,
-				BeesBookCommon::Stage::Recognizer);
+				BeesBookCommon::Stage::EllipseFitter);
 	connectSettingsWidget(paramsWidget.canny_max_mean,
 				Params::BASE + Params::CANNY_MEAN_MAX,
-				BeesBookCommon::Stage::Recognizer);
+				BeesBookCommon::Stage::EllipseFitter);
 	connectSettingsWidget(paramsWidget.canny_min_mean,
 				Params::BASE + Params::CANNY_MEAN_MIN,
-				BeesBookCommon::Stage::Recognizer);
+				BeesBookCommon::Stage::EllipseFitter);
 	connectSettingsWidget(paramsWidget.canny_value_distance,
 				Params::BASE + Params::CANNY_VALUES_DISTANCE,
-				BeesBookCommon::Stage::Recognizer);
+				BeesBookCommon::Stage::EllipseFitter);
 
 	/**
 	 * size
@@ -46,31 +46,31 @@ RecognizerParamsWidget::RecognizerParamsWidget(Settings &settings)
 
 	connectSettingsWidget(paramsWidget.max_major_axis,
 				Params::BASE + Params::MAX_MAJOR_AXIS,
-				BeesBookCommon::Stage::Recognizer);
+				BeesBookCommon::Stage::EllipseFitter);
 	connectSettingsWidget(paramsWidget.max_minor_axis,
 				Params::BASE + Params::MAX_MINOR_AXIS,
-				BeesBookCommon::Stage::Recognizer);
+				BeesBookCommon::Stage::EllipseFitter);
 	connectSettingsWidget(paramsWidget.min_major_axis,
 				Params::BASE + Params::MIN_MAJOR_AXIS,
-				BeesBookCommon::Stage::Recognizer);
+				BeesBookCommon::Stage::EllipseFitter);
 	connectSettingsWidget(paramsWidget.min_minor_axis,
 				Params::BASE + Params::MIN_MINOR_AXIS,
-				BeesBookCommon::Stage::Recognizer);
+				BeesBookCommon::Stage::EllipseFitter);
 	/**
 	 * vote
 	 */
 
 	connectSettingsWidget(paramsWidget.honey_enabled,
 				Params::BASE + Params::USE_XIE_AS_FALLBACK,
-				BeesBookCommon::Stage::Recognizer);
+				BeesBookCommon::Stage::EllipseFitter);
 	connectSettingsWidget(paramsWidget.threshold_best_vote,
 				Params::BASE + Params::THRESHOLD_BEST_VOTE,
-				BeesBookCommon::Stage::Recognizer);
+				BeesBookCommon::Stage::EllipseFitter);
 	connectSettingsWidget(paramsWidget.threshold_edge_pixels,
 				Params::BASE + Params::THRESHOLD_EDGE_PIXELS,
-				BeesBookCommon::Stage::Recognizer);
+				BeesBookCommon::Stage::EllipseFitter);
 	connectSettingsWidget(paramsWidget.threshold_vote,
 				Params::BASE + Params::THRESHOLD_VOTE,
-				BeesBookCommon::Stage::Recognizer);
+				BeesBookCommon::Stage::EllipseFitter);
 
 }
